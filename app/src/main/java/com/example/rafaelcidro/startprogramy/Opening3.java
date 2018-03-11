c0659d206171101b59b50a6597e2426988a839fe
Rafael Cidrão <rafael7n7@hotmail.com>
2018-03-11 19:51:20 -0300

    Finalização das classes Openings.

--- app/src/main/java/com/example/rafaelcidro/startprogramy/Opening3.java
+++ app/src/main/java/com/example/rafaelcidro/startprogramy/Opening3.java
@@ -1,13 +1,34 @@
 package com.example.rafaelcidro.startprogramy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Opening3 extends AppCompatActivity {
+
+    TextView opening3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening3);
+
+        opening3 = (TextView) findViewById(R.id.pular_instrucoes3);
+        opening3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Opening3.this,MainActivity.class);
+                startActivity(it);
+            }
+        });
     }
+
+    public void startActivityOp4(View view){
+        Intent activityOp4 = new Intent(this,Opening4.class);
+        startActivity(activityOp4);
+
+    }
+
 }
--- app/src/main/java/com/example/rafaelcidro/startprogramy/Opening4.java
+++ app/src/main/java/com/example/rafaelcidro/startprogramy/Opening4.java
@@ -1,13 +1,28 @@
 package com.example.rafaelcidro.startprogramy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Opening4 extends AppCompatActivity {
+
+    TextView opening4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening4);
+
+        opening4 = (TextView) findViewById(R.id.pular_instrucoes4);
+        opening4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Opening4.this,MainActivity.class);
+                startActivity(it);
+            }
+        });
+
     }
 }
